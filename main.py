76c699ba39fdd2bdbf31657cb4caad0adfb65a92
Мария Михайлова <115002526+MarijaMihailova@users.noreply.github.com>
2022-10-18 11:00:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# # # # # teksts=input ("Ievadi tekstu! ")
+# # # # # skaititajs=0
+# # # # # for burts in teksts:
+# # # # #   if burts=='c':
+# # # # #     skaititajs+=1
+# # # # # print("Burtu a skaits =", skaititajš)
+# # # # sar1=[5,45,1,45,45,12]
+# # # # garums=len(sar1)
+# # # # lielakais=max(sar1)
+# # # # mazakaiS=min(sar1)
+# # # # atkartojumi=sar1.count(45)
+# # # sar1=[5,2,1,2, 1, 12]
+# # # reizinajums=1
+# # # for elem in sar1:
+# # #   print("Tekošais elements =", elem,"Tekošais reizinājums =",reizinajums)
+# # # reizinajums =reizinajums*elem
+# # # print("Reizinājums =", reizinajums)
+# # sar1=[5,2,1,2, 1, 12]
+# # summa=0
+# # skait=1
+# # for elem in sar1:
+# #   print (sar1[len (sar1)-skait])
+# #   skait+=1
+# skait=0
+# while skait<=100 :
+#   if skait%2!=0:
+#     print(skait)
+#   skait+=1
